e10dfc491e8ac9e9ffcfa3b85a18d788d5654d9d
Brap098 <Brapagon.Work@gmail.com>
2022-09-15 21:01:33 -0400

    abouti

--- views/about.jsx
+++ views/about.jsx
@@ -7,7 +7,7 @@ function about () {
             <main>
                 <h1>Who We Are</h1>
                 <div>
-                    <img src="/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg" class="img-fluid" alt="Responsive Image" /> 
+                    <img className='abouti' src="/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg" class="img-fluid" alt="Responsive Image" /> 
                <h2>Frontend Developers</h2>
                 <ul>
                     <li> Brapagon Voyles </li>
